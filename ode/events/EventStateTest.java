80db3f7fe50f4a0e693df9e2992ae55a4e72dd73
chupanw <chupanw@cs.cmu.edu>
2015-09-06 20:53:09 -0400

    Generate test suite for bug 7(unpatched)

---
+++ ode/events/EventStateTest.java
@@ -0,0 +1,28 @@
+package cmu.defect4j.math3.ode.events;
+
+import gov.nasa.jpf.util.test.TestJPF;
+import org.junit.Test;
+
+public class EventStateTest extends TestJPF {
+
+    private final String[] config = {"+search.class= .search.RandomSearch","+nhandler.delegateUnhandledNative","+classpath+=${jpf-core}/lib/junit-4.11.jar,lib/math7b.jar"};
+    public static void main(String[] testMethods){
+        runTestsOfThisClass(testMethods);
+    }
+    @Test(timeout=1800000)
+    public void closeEvents() throws Exception {
+        if (verifyNoPropertyViolation(config)) {
+               org.apache.commons.math3.ode.events.EventStateTest object = new org.apache.commons.math3.ode.events.EventStateTest();
+               object.closeEvents();
+        }
+    }
+
+    @Test(timeout=1800000)
+    public void testIssue695() throws Exception {
+        if (verifyNoPropertyViolation(config)) {
+               org.apache.commons.math3.ode.events.EventStateTest object = new org.apache.commons.math3.ode.events.EventStateTest();
+               object.testIssue695();
+        }
+    }
+
+}
